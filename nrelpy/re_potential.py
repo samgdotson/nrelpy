97ae40d346f477adc78b6887d1a140c8127a77d6
Sam Dotson <samgdotson@gmail.com>
2022-09-11 15:04:25 -0500

    adds complete test cov for re_potential

--- nrelpy/re_potential.py
+++ nrelpy/re_potential.py
@@ -1,10 +1,9 @@
 from urllib.error import HTTPError
 import pandas as pd
 import warnings
-# warnings.filterwarnings("ignore")
 
 
-def as_dataframe(verbose=False, **kwargs):
+def as_dataframe(url=None, verbose=False, **kwargs):
     """
     This function downloads the specified Annual Technology Baseline Dataset.
     If this data is used in a research publication, users should cite:
@@ -18,17 +17,20 @@ def as_dataframe(verbose=False, **kwargs):
     df : pandas.DataFrame
         The United States Renewable Energy Technical Potential dataset as a pandas dataframe.
     """
-    url = "https://www.nrel.gov/gis/assets/docs/us-re-technical-potential.xlsx"
+    if url:
+        URL = url
+    else:
+        URL = "https://www.nrel.gov/gis/assets/docs/us-re-technical-potential.xlsx"
 
     try:
         print(f'Downloading Renewable Energy Technical Potential')
         if not verbose:
             warnings.simplefilter(action='ignore', category=UserWarning)
-        df = pd.read_excel(url, sheet_name='Data', skiprows=1, index_col='State')
+        df = pd.read_excel(URL, sheet_name='Data', skiprows=1, index_col='State')
         print('Download Successful.')
 
     except HTTPError as err:
-        fail_str = (f'Failed to download from URL: {url}.')
+        fail_str = (f'Failed to download from URL: {URL}.')
         print(err.code, fail_str)
         raise
 
