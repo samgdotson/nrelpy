12f5c3e693c84fec842a6aa435da0594a22c6b47
Sam Dotson <samgdotson@gmail.com>
2022-09-12 18:06:23 -0500

    adds class for ATBE and some tests

--- nrelpy/atb.py
+++ nrelpy/atb.py
@@ -57,3 +57,110 @@ def as_dataframe(year, database, verbose=False, **kwargs):
         save_local(df, database=database, year=year)
 
     return df
+
+
+class ATBe(object):
+    """
+    A class that allows simplified access to the various
+    cases and data values.
+    """
+
+    def __init__(
+            self,
+            year,
+            database='electricity',
+            case='R&D',
+            scenario='Moderate',
+            crp = '20',
+            **kwargs) -> None:
+        """
+        Initializes the ATB class.
+
+        Parameters
+        ----------
+        year : int
+            Specifies the ATB year
+        database : string
+            Specifies which ATB database. 
+            Accepts ['electricity', 'transportation']
+            Default is 'electricity'.
+        case : string
+            Specifies the technology case. 
+            Accepts ['Market', 'R&D']
+        scenario : string
+            Specifies the technology scenario. 
+            Accepts ['Conservative', 'Moderate', 'Advanced']
+        crp : string or int
+            `crp` stands for "cost recovery period." This parameter only
+            influences the levelized cost of electricity (LCOE) 
+            calculation. Default is 20 years.
+        """
+
+        self.year = year
+        self.database = database
+        self.case = case
+        self.scenario = scenario
+        self.crp = crp
+
+
+    @property
+    def dataframe(self):
+        """
+        Creates a subset of the ATBe according to the settings
+        stored as class attributes. Updates whenever a setting
+        is changed.
+        
+        >>> atbe = ATBe(2022)
+        >>> print(atbe.dataframe.scenario.unique())
+        ['Moderate']
+        >>> atbe.scenario = 'Advanced'
+        >>> print(atbe.dataframe.scenario.unique())
+        ['Advanced']
+        """
+        df = as_dataframe(year=self.year, database=self.database)
+        df = df[(df.core_metric_case == self.case) &
+                (df.scenario == self.scenario) &
+                (df.crpyears) == str(self.crp)]
+        return df
+
+
+    def get_data(self, 
+                technology, 
+                core_metric_param, 
+                techdetail,
+                value_only=True):
+        """
+        Retrieves a specific piece of technology data.
+
+        Parameters
+        ----------
+        technology : string
+            Specifies the technology. 
+        core_metric_param : string
+            The parameter of interest.
+            Accepts ['CAPEX', 'Fixed O&M', 'Variable O&M', 
+                    'Fuel', 'CF', 'LCOE']
+        techdetail : string
+            A more descriptive parameter to narrow down
+            technology selection. For example, `LandbasedWind`
+            has data for multiple wind classes (e.g. `Class9`).
+            This must be specified in order to distinguish values.
+        """
+
+        self.dataframe
+
+
+
+if __name__ == "__main__":
+
+    
+    year = 2022
+    atb_class = ATBe(year=2022)
+
+    print(atb_class.scenario)
+
+    print(atb_class.dataframe.scenario.unique())
+
+    atb_class.scenario = 'Advanced'
+
+    print(atb_class.dataframe.scenario.unique())
--- nrelpy/tests/test_atb.py
+++ nrelpy/tests/test_atb.py
@@ -1,4 +1,5 @@
-from nrelpy.atb import *
+from nrelpy.atb import as_dataframe, ATBe
+from urllib.error import HTTPError
 import pytest
 
 good_year = 2020
@@ -74,3 +75,28 @@ def test_as_dataframe_bad_database():
     assert (e.type == KeyError)
 
     return
+
+
+def test_ATB_init():
+
+    atb_class = ATB(year=2022)
+
+    assert atb_class.scenario == 'Moderate'
+    assert atb_class.case == 'R&D'
+    assert atb_class.year == 2022
+    assert atb_class.dataframe.scenario.unique() == ['Moderate']
+
+    return
+
+def test_ATB_scenario_change():
+    
+    atb_class = ATBe(year=2022)
+
+    assert atb_class.dataframe.scenario.unique() == ['Moderate']
+    assert atb_class.dataframe.core_metric_case.unique() == ['R&D']
+
+    atb_class.scenario = 'Advanced'
+    atb_class.case = 'Market'
+    assert atb_class.dataframe.scenario.unique() == ['Advanced']
+    assert atb_class.dataframe.core_metric_case.unique() == ['Market']
+
